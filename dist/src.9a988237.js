bc548e8bf1d18992dcd0799e4bba365a00080b42
Nick Zart <nzart@g.clemson.edu>
2019-06-25 18:35:39 -0400

    UPDATE: We will see

--- dist/src.9a988237.js
+++ dist/src.9a988237.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"H99C":[function(require,module,exports) {
 "use strict";var e=r(require("bricks.js"));function r(e){return e&&e.__esModule?e:{default:e}}var t=(0,e.default)({container:"#brick",packed:"data-packed",sizes:[{columns:2,gutter:50},{mq:"1440px",columns:3,gutter:40}]});document.querySelector("#brick").style.margin="0 auto",t.resize(!0),t.pack();
 },{"bricks.js":"63XF"}]},{},["H99C"], null)
-//# sourceMappingURL=/nzart/src.9a988237.js.map
+//# sourceMappingURL=/nzart.github.io/src.9a988237.js.map
