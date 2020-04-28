da5846730fbcf4cd8bd70f9648c4c86ead6fffdc
Nick Zart <nzart@g.clemson.edu>
2020-04-28 19:39:10 -0400

    UPDATE: 04.28.2020

--- script.js
+++ script.js
@@ -1,25 +1,49 @@
-var nav = document.querySelector('.nav');
-var logo = document.querySelector('.logo');
-var first = document.querySelector('.first');
-var second = document.querySelector('.second');
-var third = document.querySelector('.third');
-window.onscroll = function () { 
+let nav = document.querySelector('.nav');
+let logo = document.querySelector('.logo');
+let first = document.querySelector('.first');
+let second = document.querySelector('.second');
+let third = document.querySelector('.third');
+
+
+window.onscroll = () => { 
     "use strict";
     if (window.pageYOffset >= 200 ) {
-        nav.style.backgroundColor = '#000';
-        logo.style.color = '#fff';
-        first.style.color = '#fff';
-        second.style.color = '#fff';
-        third.style.color = '#fff';
+        nav.style.backgroundColor = '#050505';
+        logo.style.color = '#fcfcfc';
+        first.style.color = '#fcfcfc';
+        second.style.color = '#fcfcfc';
+        third.style.color = '#fcfcfc';
     } 
     else {
 //        nav.style.backgroundColor = 'rgba(255, 255, 255, 0)';
-        nav.style.backgroundColor = '#fff';
+        nav.style.backgroundColor = '#fcfcfc';
         logo.style.color = '#6414c1';
-        first.style.color = '#000';
-        second.style.color = '#000';
-        third.style.color = '#000';
+        first.style.color = '#050505';
+        second.style.color = '#050505';
+        third.style.color = '#050505';
     }
+    
+    let rotate = document.getElementById('rotate');
+  
+    let scroll = ((window.pageYOffset) / 4);
+  
+    let nev = -Math.abs(scroll);
+  
+    rotate.style.transform = `rotate(${nev}deg)`;
 };
 
+
+
+
+ScrollReveal().reveal('h1', {
+    delay: 500
+});
+
+ScrollReveal().reveal('p', {
+    delay: 500
+});
+
+ScrollReveal().reveal('img', {
+    delay: 500
+})
 
